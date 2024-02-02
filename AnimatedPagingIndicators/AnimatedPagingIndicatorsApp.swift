6c9ba0e3d1578e9a5ef70a42b97dbfd2fd08bf64
Amish <amish.tufail2002@gmail.com>
2024-02-02 11:41:44 +0500

    Initial Commit

---
+++ AnimatedPagingIndicators/AnimatedPagingIndicatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedPagingIndicatorsApp.swift
+//  AnimatedPagingIndicators
+//
+//  Created by Amish Tufail on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedPagingIndicatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
